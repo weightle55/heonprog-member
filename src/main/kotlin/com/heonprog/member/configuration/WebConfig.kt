68db8a48c74e445f4acdcbab5a2665443abdc590
weightle55 <metalicboots@gmail.com>
2023-12-07 02:30:44 +0900

    tailwind 롤백

---
+++ src/main/kotlin/com/heonprog/member/configuration/WebConfig.kt
@@ -0,0 +1,13 @@
+package com.heonprog.member.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+/**
+ * Created on 2023-12-05
+ *
+ * @author weightle55
+ */
+@Configuration
+class WebConfig : WebMvcConfigurer {
+}
